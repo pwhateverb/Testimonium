c4390f0953062201f7c07c5fdab755670ada8551
AliMousa27 <jafar.myhero@gmail.com>
2024-04-19 01:16:57 +0200

    inital commit

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <!-- Background image -->
+    <div id="navbar">
+      <span id="home">Home</span>
+      <span id="requirements">Requirements</span>
+    </div>
+    <div id="line"></div>
+    <img id="logo" src="./assets/idk.png" />
+    <div id="header">
+      <h1 class="text-center text-white">Testimonium</h1>
+      <p class="text-center sub-header">
+        Testimonium is a platform that allows you to create and share your
+        testimonies with the world.
+      </p>
+    </div>
+    <button type="button" class="btn btn-outline-light txt_reqs">
+      Write<br />Requirements
+    </button>
+    <button type="button" class="btn btn-danger upload_pdf">Upload PDF</button>
+
     <div
       id="background"
       class="p-5 text-center bg-image"
       :style="{
         backgroundImage: `url(${backgroundImage})`,
       }"
-    >
-      <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
-        <div class="d-flex justify-content-center align-items-center h-100">
-          <div class="text-white">
-            <h1 class="mb-3">Heading</h1>
-            <h4 class="mb-3">Subheading</h4>
-            <a
-              data-mdb-ripple-init
-              class="btn btn-outline-light btn-lg"
-              href="#!"
-              role="button"
-              >Call to action</a
-            >
-          </div>
-        </div>
-      </div>
+    ></div>
+    <div class="jumbotron">
+      <h2 class="display-4">Product benefits</h2>
+      <p class="lead">
+        <br />Testamonium is a platform that allows you to fully automate the
+        <br />process of writing detailed test specifcations from your
+        requirements.
+      </p>
+      <hr class="my-4" />
+      <p>
+        It uses utility classes for typography and spacing to space content out
+        within the larger container.
+      </p>
     </div>
   </div>
 </template>
@@ -32,18 +46,74 @@ export default {
   name: "App",
   data() {
     return {
-      backgroundImage: require("@/assets/t.png"),
+      backgroundImage: require("@/assets/background.png"),
     };
   },
 };
 </script>
 
 <style>
+#navbar {
+  position: absolute;
+  top: 15px;
+  right: 10px;
+}
+
+#navbar span {
+  margin-right: 20px;
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+}
+
 #background {
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   height: 400px;
   width: 100%;
+}
+
+#line {
+  position: absolute;
+  top: 70px;
+  height: 1px;
+  background: white;
+  width: 100%;
+}
+
+#logo {
+  position: absolute;
+  top: 15px;
+  margin-left: 40px;
+  width: 240px;
+  cursor: pointer;
+}
+
+#header {
+  position: absolute;
+  top: 100px;
+  width: 100%;
+}
+
+.sub-header {
+  color: rgb(234, 234, 234);
+}
+
+.txt_reqs {
+  position: absolute;
+  top: 230px;
+
+  left: 44%;
+  transform: translateX(-50%);
+}
+
+.upload_pdf {
+  height: 60px;
+  width: 125px;
+  position: absolute;
+  top: 230px;
+  left: 56%;
+  transform: translateX(-50%);
 }
 </style>
