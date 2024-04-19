c28c0b0888742ffb4cb4a648acd24ec49ded9cb9
parisab <parisab@student.chalmers.se>
2024-04-19 14:45:37 +0200

    Add pdfjs-dist dependency and update Home.vue to handle PDF uploads
    Co-authored-by: @Adam1927

--- test/src/Home.vue
+++ test/src/Home.vue
@@ -1,25 +1,87 @@
 <template>
   <div id="app">
-    <div id="navbar">
-      <span id="home">Home</span>
-      <span id="requirements">Requirements</span>
-    </div>
-    <div id="line"></div>
     <img id="logo" src="./assets/logo.png" />
     <div id="header">
-      <h1 class="text-center text-white">Testimonium</h1>
       <p class="text-center sub-header">
         Testimonium is a platform that allows you to create and share your
         testimonies with the world.
       </p>
     </div>
-    <button type="button" class="btn btn-outline-light txt_reqs">
+    <button
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+      type="button"
+      class="btn btn-outline-light txt_reqs"
+    >
       Write<br />Requirements
     </button>
-    <div @click="generate()">
-      <button type="button" class="btn btn-danger upload_pdf">
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              Write your requirements
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <textarea
+              v-model="requirements"
+              style="height: 300px"
+              class="form-control"
+              aria-describedby="emailHelp"
+            ></textarea>
+          </div>
+          <div
+            class="modal-footer"
+            style="display: inline-block; text-align: center"
+          >
+            <input
+              type="file"
+              id="fileUpload"
+              accept=".pdf"
+              style="display: none"
+              @change="handleFileUpload"
+            />
+
+            <button
+              @click="handleUpload()"
+              type="button"
+              class="btn btn-primary"
+              style="width: 150px"
+            >
+              Upload
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <button
+        @click="triggerFileUpload"
+        type="button"
+        class="btn btn-danger upload_pdf"
+      >
         Upload PDF
       </button>
+      <input
+        type="file"
+        ref="pdfUploader"
+        @change="handleFileUpload"
+        style="display: none"
+        accept=".pdf"
+      />
     </div>
     <div
       id="background"
@@ -36,6 +98,12 @@
         requirements.
       </p>
       <hr class="my-4" />
+      <img
+        v-if="isLoading"
+        src="./assets/loading.gif"
+        alt="loading"
+        id="loading"
+      />
       <p>
         It uses utility classes for typography and spacing to space content out
         within the larger container.
@@ -46,20 +114,21 @@
   
   <script>
 import axios from "axios";
+import { getDocument } from "pdfjs-dist/webpack";
 
 export default {
   name: "home-page",
   data() {
     return {
       backgroundImage: require("@/assets/background.png"),
-      requirements:
-        "FR1: The user shall be able to view and manage plants" +
-        "FR2: The system shall provide onboarding to show how to use the app",
+      isLoading: false,
+      requirements: "",
     };
   },
   methods: {
-    generate() {
-      const encodedRequirements = encodeURIComponent(this.requirements);
+    generate(requirements) {
+      this.isLoading = true;
+      const encodedRequirements = encodeURIComponent(requirements);
       axios
         .get("http://127.0.0.1:5000?requirements=" + encodedRequirements)
         .then((response) => {
@@ -71,24 +140,46 @@ export default {
           console.error("Error making request:", error);
         });
     },
+    triggerFileUpload() {
+      this.$refs.pdfUploader.click();
+      this.isLoading = true;
+    },
+    async handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const message = await new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.onload = async (e) => {
+            const typedarray = new Uint8Array(e.target.result);
+            const pdf = await getDocument(typedarray).promise;
+
+            // Loop over all pages and extract text
+            let text = "";
+            for (let i = 1; i <= pdf.numPages; i++) {
+              const page = await pdf.getPage(i);
+              const textContent = await page.getTextContent();
+              text += textContent.items.map((item) => item.str).join(" ");
+            }
+
+            resolve(text);
+          };
+          reader.readAsArrayBuffer(file);
+        });
+
+        console.log("Message:", message);
+        this.generate(message);
+      }
+    },
+    handleUpload() {
+      const uploadedRequirements = this.requirements;
+      this.generate(uploadedRequirements);
+      this.requirements = "";
+    },
   },
 };
 </script>
   
   <style>
-#navbar {
-  position: absolute;
-  top: 15px;
-  right: 10px;
-}
-
-#navbar span {
-  margin-right: 20px;
-  font-size: 20px;
-  color: white;
-  cursor: pointer;
-}
-
 #background {
   background-size: cover;
   background-repeat: no-repeat;
@@ -97,25 +188,17 @@ export default {
   width: 100%;
 }
 
-#line {
-  position: absolute;
-  top: 70px;
-  height: 1px;
-  background: white;
-  width: 100%;
-}
-
 #logo {
   position: absolute;
-  top: 15px;
-  margin-left: 40px;
-  width: 240px;
+  top: 13%;
+  left: 40%;
+  width: 340px;
   cursor: pointer;
 }
 
 #header {
   position: absolute;
-  top: 100px;
+  top: 20%;
   width: 100%;
 }
 
@@ -139,5 +222,18 @@ export default {
   left: 56%;
   transform: translateX(-50%);
 }
+
+.jumbotron {
+  background-color: #162a33;
+  color: white;
+  margin: 50px;
+  padding: 10px;
+}
+
+#loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
-  
